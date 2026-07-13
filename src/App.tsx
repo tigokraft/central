5d28eb2a1c03a60a80263249b55ccaed13f6e9f9
tigokraft <tiagoco2008@gmail.com>
2026-07-13 01:43:42 +0100

    feat: render CustomTitleBar at the top of App layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Topbar from "./components/Topbar";
 import Sidebar from "./components/Sidebar";
 import InfiniteCanvas from "./components/canvas/InfiniteCanvas";
+import CustomTitleBar from "./components/CustomTitleBar";
 import { useCanvasStore } from "./store/canvasStore";
 
 export default function App() {
@@ -19,21 +20,24 @@ export default function App() {
     }));
 
   return (
-    <div className="flex h-screen w-screen bg-slate-950 text-slate-50 font-sans overflow-hidden">
-      <Sidebar
-        onLoadPreset={loadPreset}
-        activeProcesses={activeProcesses}
-      />
-      <div className="flex-1 flex flex-col min-w-0 bg-slate-900 relative">
-        <Topbar
-          showMinimap={showMinimap}
-          setShowMinimap={setShowMinimap}
+    <div className="flex flex-col h-screen w-screen bg-slate-950 text-slate-50 font-sans overflow-hidden">
+      <CustomTitleBar />
+      <div className="flex flex-1 overflow-hidden min-h-0">
+        <Sidebar
+          onLoadPreset={loadPreset}
+          activeProcesses={activeProcesses}
         />
-        <div className="flex-1 relative">
-          <InfiniteCanvas
+        <div className="flex-1 flex flex-col min-w-0 bg-slate-900 relative">
+          <Topbar
             showMinimap={showMinimap}
             setShowMinimap={setShowMinimap}
           />
+          <div className="flex-1 relative">
+            <InfiniteCanvas
+              showMinimap={showMinimap}
+              setShowMinimap={setShowMinimap}
+            />
+          </div>
         </div>
       </div>
     </div>
