d056ca26d5585576be94d0a5f22fd225d8ee7c7e
Tiago Oliveira <tiagoco2008@gmail.com>
2026-07-17 20:14:41 +0100

    wire App.tsx view switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
-import { useState } from "react";
-import Topbar from "./components/Topbar";
-import Sidebar from "./components/Sidebar";
-import InfiniteCanvas from "./components/canvas/InfiniteCanvas";
-import OrchestratorBar from "./components/canvas/OrchestratorBar";
 import CustomTitleBar from "./components/CustomTitleBar";
-import { useCanvasStore } from "./store/canvasStore";
+import HomeView from "./components/home/HomeView";
+import CanvasWorkspaceView from "./components/canvas/CanvasWorkspaceView";
+import { useAppViewStore } from "./store/appViewStore";
 
 export default function App() {
-  const nodes = useCanvasStore((state) => state.nodes);
-  const loadPreset = useCanvasStore((state) => state.loadPreset);
-  const [showMinimap, setShowMinimap] = useState(false);
-
-  // Sync active processes with Sidebar monitor
-  const activeProcesses = nodes
-    .filter((n) => n.type === "terminalNode")
-    .map((n) => ({
-      id: n.id,
-      label: n.data.label,
-      isRunning: !!n.data.isRunning,
-    }));
+  const view = useAppViewStore((state) => state.view);
 
   return (
     <div className="flex flex-col h-screen w-screen bg-slate-950 text-slate-50 font-sans overflow-hidden">
       <CustomTitleBar />
-      <div className="flex flex-1 overflow-hidden min-h-0">
-        <Sidebar
-          onLoadPreset={loadPreset}
-          activeProcesses={activeProcesses}
-        />
-        <div className="flex-1 flex flex-col min-w-0 bg-slate-900 relative">
-          <Topbar
-            showMinimap={showMinimap}
-            setShowMinimap={setShowMinimap}
-          />
-          <OrchestratorBar />
-          <div className="flex-1 relative">
-            <InfiniteCanvas
-              showMinimap={showMinimap}
-            />
-          </div>
-        </div>
-      </div>
+      {view === "home" ? <HomeView /> : <CanvasWorkspaceView />}
     </div>
   );
 }
